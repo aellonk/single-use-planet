0ed3af6c2d2007e663c13e6128e8fac6f0153897
Aellon Krider <aellonkrider@gmail.com>
2019-07-17 18:10:22 -0400

    Refactor Items.js by separating ItemCard into own component

--- single-use-planet-client/src/containers/Items.js
+++ single-use-planet-client/src/containers/Items.js
@@ -1,17 +1,10 @@
 import React from 'react';
+import ItemCard from '../components/ItemCard'
 
 const Items = (props) => (
 	<div>
 		<h3>What To Do With:</h3>
-		{props.items.map(item => 
-			<div key={item.id}> 
-				<h1>{item.name}</h1>
-				<img src={item.img_url} alt={item.name}/>
-				<p>Material: {item.material}</p>
-				<p>Alternative Options: {item.alternative}</p>
-				<p>How To Recycle: {item.instructions}</p>
-			</div>
-		)}
+		{props.items.map(item => <ItemCard item={item} />)}
 	</div>
 )
 
